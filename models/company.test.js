55ff92ca5619a864b06c4a100ec1c7ca786a17a6
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-07-14 10:50:45 -0700

    modified GET/companies/:handle to include all jobs associated with company

--- models/company.test.js
+++ models/company.test.js
@@ -174,6 +174,14 @@ describe("get", function () {
       description: "Desc1",
       numEmployees: 1,
       logoUrl: "http://c1.img",
+      jobs: [
+        {
+          id: 1,
+          title: "j1",
+          salary: 70000,
+          equity: "0"
+        }
+      ]
     });
   });
 
